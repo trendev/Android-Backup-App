e7d78660ff4570a19a342eab891a0e06ec9a06f3
jsie <julien.sie@gmail.com>
2016-05-25 19:04:16 +0200

    interactive message

--- app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
+++ app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +20,11 @@ public class PremiereActivite extends AppCompatActivity {
     public void sayHello(View v) {
         final TextView textView = (TextView) findViewById(R.id.textView);
         assert textView != null;
-        textView.setText(R.string.helloworld);
-        Toast.makeText(this,R.string.helloworld,Toast.LENGTH_SHORT).show();
+        final EditText editText = (EditText) findViewById(R.id.firstname);
+        assert editText != null;
+        final String firstname = editText.getText().toString();
+        String message = getResources().getString(R.string.helloworld, firstname);
+        textView.setText(message);
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
