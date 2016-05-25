5c7a6c4ec1e7ba4b90fa816d9321a3fe055fc47f
jsie <julien.sie@gmail.com>
2016-05-25 10:59:25 +0200

    additionnal languages

--- app/src/test/java/com/openclassrooms/fr/premierprojet/ExampleUnitTest.java
+++ app/src/test/java/com/openclassrooms/fr/premierprojet/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.openclassrooms.fr.premierprojet;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,5 +14,24 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void total_isCorrect() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        assertTrue(list.isEmpty());
+        int value = 0;
+        for (int i = 0 ; i < 10 ; i++){
+            value += i;
+            list.add(value);
+        }
+
+        for(int  i : list)
+            System.out.println(i);
+
+        assertEquals(value,45);
+        assertFalse(list.isEmpty());
+
+
     }
 }
