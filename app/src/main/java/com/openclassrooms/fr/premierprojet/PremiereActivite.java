c46b7aa7901ce7e281af81e46429525027946414
jsie <julien.sie@gmail.com>
2016-05-25 13:08:46 +0200

    layering bis

--- app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
+++ app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
@@ -2,18 +2,19 @@ package com.openclassrooms.fr.premierprojet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PremiereActivite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /**
-         * It's possible to add a simple TextView but it's recommanded to use xml configuration
-         * instead.
-         */
+
         setContentView(R.layout.activity_premiere_activite);
         String hello = getResources().getString(R.string.helloworld);
-        System.out.println(hello.concat(" I'm happy to meet you;) "));
+        System.out.println(hello.concat(" I'm happy to meet you;) "));//display on terminal output
+        TextView textView = (TextView) findViewById(R.id.textView);
+        if (textView != null)
+            textView.setText(textView.getText().toString().concat("\nIt's a pleasure."));
     }
 }
