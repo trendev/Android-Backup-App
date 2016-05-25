3576264c81f278ebf41df0087d59d8abd020c8d6
jsie <julien.sie@gmail.com>
2016-05-25 15:57:26 +0200

    add button in order to display the hello message

--- app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
+++ app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
@@ -2,6 +2,8 @@ package com.openclassrooms.fr.premierprojet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PremiereActivite extends AppCompatActivity {
@@ -11,10 +13,20 @@ public class PremiereActivite extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_premiere_activite);
-        String hello = getResources().getString(R.string.helloworld);
-        System.out.println(hello.concat(" I'm happy to meet you;) "));//display on terminal output
-        TextView textView = (TextView) findViewById(R.id.textView);
-        if (textView != null)
-            textView.setText(textView.getText().toString().concat("\nIt's a pleasure."));
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    if (textView != null) {
+                        textView.setText(R.string.helloworld);
+                    }
+                }
+            });
+        }
     }
 }
