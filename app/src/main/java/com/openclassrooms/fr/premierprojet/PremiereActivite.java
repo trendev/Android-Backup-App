76449a7fef1988fc7581583ad437deed2ece885a
jsie <julien.sie@gmail.com>
2016-05-26 17:54:14 +0200

    add envs

--- app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
+++ app/src/main/java/com/openclassrooms/fr/premierprojet/PremiereActivite.java
@@ -2,25 +2,35 @@ package com.openclassrooms.fr.premierprojet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Map;
 
 public class PremiereActivite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_premiere_activite);
+
+        /**
+         * restore the data saved in onRetainNonConfigurationInstance()
+         * getLastCustomNonConfigurationInstance();
+         */
+
     }
 
     public void sayHello(View v) {
         final TextView textView = (TextView) findViewById(R.id.textView);
         assert textView != null;
+        textView.setTextSize(32f);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
         final EditText editText = (EditText) findViewById(R.id.firstname);
         assert editText != null;
         final String firstname = editText.getText().toString();
@@ -37,5 +47,21 @@ public class PremiereActivite extends AppCompatActivity {
 
         textView.setText(text);
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayEnv(View v) {
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        assert textView != null;
+        textView.setTextSize(10f);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+
+        Map<String, String> envs = System.getenv();
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> e : envs.entrySet())
+            sb.append(e.getKey() + " -- " + e.getValue() + "\n");
+
+        textView.setText(sb.toString());
     }
 }
