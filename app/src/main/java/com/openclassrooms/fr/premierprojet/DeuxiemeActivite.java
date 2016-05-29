55842e282b380de40ca494565ad5902e3717abe6
jsie <julien.sie@gmail.com>
2016-05-29 09:13:42 +0200

    format comments

--- app/src/main/java/com/openclassrooms/fr/premierprojet/DeuxiemeActivite.java
+++ app/src/main/java/com/openclassrooms/fr/premierprojet/DeuxiemeActivite.java
@@ -23,7 +23,10 @@ public class DeuxiemeActivite extends AppCompatActivity {
     private final String filename = "android.txt";
     private final String userpwd = "jsie:qsec0fr";
 
-private Thread t;
+    /**
+     * Thread where exploration will be processed
+     */
+    private Thread t;
 
     private int total = 0;
 
@@ -57,7 +60,7 @@ private Thread t;
             sb.append(process.pid).append(" -- ").append(process.processName).append("\n")*/
 
         //TODO: insert the exploration result in a ListView and display a progress bar
-//TODO: use an ArrayAdapter<SmbFile> and a SmbFileHolder
+        //TODO: use an ArrayAdapter<SmbFile> and a SmbFileHolder
         t = new Thread(new Runnable() {
             @Override
             public void run() {
