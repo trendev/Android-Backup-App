98c09ba16e28f58d2a2c1571eb822e74250512e4
jsie <julien.sie@gmail.com>
2016-05-31 19:34:02 +0200

    Playing with Intent and PendingIntent...

--- app/src/test/java/com/openclassrooms/fr/premierprojet/ExampleUnitTest.java
+++ app/src/test/java/com/openclassrooms/fr/premierprojet/ExampleUnitTest.java
@@ -2,16 +2,40 @@ package com.openclassrooms.fr.premierprojet;
 
 import org.junit.Test;
 
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
+
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
 
+    private final String path = "smb://ylalsrv01/jsie-home/";
+    private final String filename = "android.txt";
+    private final String userpwd = "jsie:qsec0fr";
 
     @Test
     public void testSizeFormat() throws Exception {
         long size = 1299973064;
         String s = SmbFileAdapter.formatSize(size);
         System.out.println(s);
+    }
+
+    @Test
+    public void testSambaConnexion() throws Exception {
+
+        NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(userpwd);
+        SmbFile file = new SmbFile(path, auth);
+        assert file.canRead();
+        assert file.isDirectory();
+        int attributs = file.getAttributes();
+
+        assert (attributs & SmbFile.ATTR_READONLY) == 0;
+        System.out.println("Attributes = " + attributs);
+    }
+
+    @Test
+    public void testLocalFileExplorer() {
+
     }
 }
