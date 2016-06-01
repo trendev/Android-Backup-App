707afdfa8c483c8f01410239dc0b991eb9a81451
jsie <julien.sie@gmail.com>
2016-06-01 12:40:53 +0200

    Playing with Contacts

--- app/src/androidTest/java/com/openclassrooms/fr/premierprojet/ApplicationTest.java
+++ app/src/androidTest/java/com/openclassrooms/fr/premierprojet/ApplicationTest.java
@@ -1,12 +1,18 @@
 package com.openclassrooms.fr.premierprojet;
 
 import android.app.Application;
+import android.content.ContentProviderClient;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.test.ApplicationTestCase;
-import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.Suppress;
+import android.util.Log;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 import jcifs.smb.NtlmPasswordAuthentication;
 import jcifs.smb.SmbFile;
@@ -26,7 +32,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    @LargeTest
+    @Suppress
     public void testSambaConnexion() throws Exception {
 
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(userpwd);
@@ -36,8 +42,8 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         assertNotNull(getContext());
     }
 
+    @Suppress
     //TODO: test the remote copy on samba-cifs server
-    @LargeTest
     public void testRemoteCopy() throws Exception {
         //TODO: get a set of local file and copy them to the remove server
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(userpwd);
@@ -70,7 +76,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
     }
 
-    @LargeTest
+    @Suppress
     public void testExploreFiles() throws Exception {
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(userpwd);
         SmbFile file = new SmbFile(path, auth);
@@ -97,5 +103,54 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
             }
         }
     }
+
+    @Suppress
+    public void testFetchContacts() throws Exception {
+        /**
+         *  This array Will be used in order to map the required entries from the Contacts.
+         */
+        String[] PROJECTION = new String[]{
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+
+        /**
+         * This structure will be used to store the different entries got from the queries.
+         */
+        Map<String, String> rep = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return s.compareTo(t1);
+            }
+        });
+
+        String TAG = "FETCH_CONTACTS";
+
+        Log.i(TAG, "FETCHING...");
+
+        ContentProviderClient client = getContext().getContentResolver().acquireContentProviderClient(ContactsContract.Contacts.CONTENT_URI);
+
+        //        Cursor cursor = client.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PROJECTION, null, null, null);
+        Cursor cursor = client.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+
+        if (cursor != null && cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                rep.put(id, name);
+
+                Log.i(TAG, "Position (" + cursor.getPosition() + ") , ID = " + id + " , NAME = " + name);
+            }
+        }
+
+        Log.i(TAG, "#Contacts = " + cursor.getCount() + " vs " + rep.size() + " in map");
+
+        cursor.close();
+    }
+
 
 }
